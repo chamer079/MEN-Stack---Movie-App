067d39b50adf7c7fefbb0975d6f622f31a64c9a4
chamer79 <chamer79@gmail.com>
2024-09-24 20:07:59 -0400

    created second schema (1:M) - actorSchema

--- models/Movie.js
+++ models/Movie.js
@@ -3,11 +3,16 @@ const mongoose = require("mongoose")
 
 
 // Schema
+const actorSchema = new mongoose.Schema({
+    actor: { type: String, required: true }
+})
+
+
 const movieSchema = new mongoose.Schema({
     movieImg: { type: String, required: true },
     title: { type: String, required: true },
     director: { type: String, required: true },
-    actors: { type: String, required: true },
+    actors: [ actorSchema ],
     runningTime: { type: String, required: true },
     genre: { type: String, required: true },
     rating: { type: String, required: true },
